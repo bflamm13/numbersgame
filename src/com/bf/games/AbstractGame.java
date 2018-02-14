4868f98c947861f1a873cf842501bc797910a24a
barry flamm <barryflamm@outlook.com>
2018-02-14 14:55:02 -0500

    Update AbstractGame.java

--- src/com/bf/games/AbstractGame.java
+++ src/com/bf/games/AbstractGame.java
@@ -3,10 +3,13 @@ package com.bf.games;
 import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
+import com.bf.generator.AbstractGenerator;
+
 public abstract class AbstractGame 
 {
 	static public int DEF_PICKS = 1;
 
+	protected AbstractGenerator gen;
 	protected Game gInfo;
 	
 	abstract public Pair<List<Integer>, List<Integer>> getNumbers() throws Exception;
@@ -15,11 +18,17 @@ public abstract class AbstractGame
 	{
 		this.gInfo = g;
 	}
-	public void display(String title, int maxBalls, int maxPicks, int maxBonusBalls)
+	
+	public void set(AbstractGenerator g)
+	{
+		this.gen = g;
+	}
+	
+	public void display(String title, int maxNums, int maxPicks, int maxBonusNums)
 	{
 		System.out.println("*************"+ title +"*************");
-		System.out.println("Pick "+ maxPicks +" numbers 1 to "+ maxBalls);
-		System.out.println("Pick "+ DEF_PICKS +" bonus number 1 to "+ maxBonusBalls);
+		System.out.println("Pick "+ maxPicks +" numbers 1 to "+ maxNums);
+		System.out.println("Pick "+ DEF_PICKS +" bonus number 1 to "+ maxBonusNums);
 		System.out.println("");
 	}
 
